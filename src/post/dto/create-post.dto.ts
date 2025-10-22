b16c026ac128a4639f3a2d8ffc2997b4117329a0
kimsungwon318 <sungwon.kim@narimoto.co.jp>
2025-10-22 09:26:28 +0900

    crud add

---
+++ src/post/dto/create-post.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreatePostDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  content?: string;
+
+  @IsOptional()
+  @IsNumber()
+  authorId?: number;
+}
